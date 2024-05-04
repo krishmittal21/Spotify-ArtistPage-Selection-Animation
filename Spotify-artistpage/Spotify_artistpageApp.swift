c8f96f09c2fc8ccc9c12810cc1e8610f23203c13
Krish Mittal <krishmittal212005@gmail.com>
2024-05-04 20:32:40 +0530

    Initial Commit

---
+++ Spotify-artistpage/Spotify_artistpageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_artistpageApp.swift
+//  Spotify-artistpage
+//
+//  Created by Krish Mittal on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_artistpageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
