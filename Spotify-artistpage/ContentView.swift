8b6dbfae629d26ccfddfea61f6526b347e05c584
Krish Mittal <krishmittal212005@gmail.com>
2024-05-04 21:24:25 +0530

    select animation

--- Spotify-artistpage/ContentView.swift
+++ Spotify-artistpage/ContentView.swift
@@ -8,12 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedOption = "Music"
+    @Namespace private var namespace
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            ZStack(alignment: .bottomLeading) {
+                Image("Krish")
+                    .resizable()
+                    .ignoresSafeArea(edges: .all)
+                    .frame(height: 300)
+                
+                Text("Krish Mittal")
+                    .bold()
+                    .font(.system(.title, design: .rounded))
+                    .foregroundStyle(.white)
+                    .padding()
+            }
+            
+            Text("60M monthly listeners")
+                .font(.system(.subheadline, design: .rounded))
+                .padding(.top,10)
+            
+            HStack(spacing: 20) {
+                ZStack(alignment: .bottom) {
+                    Text("Music")
+                        .padding(.bottom,10)
+                        .foregroundStyle(.white)
+                        .frame(minWidth: 50)
+                    if selectedOption == "Music" {
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .matchedGeometryEffect(id: "Rectangle", in: namespace)
+                            .frame(width: 50, height: 2)
+                            .foregroundStyle(.green)
+                    }
+                }
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        selectedOption = "Music"
+                    }
+                }
+                
+                ZStack(alignment: .bottom) {
+                    Text("Clips")
+                        .padding(.bottom,10)
+                        .foregroundStyle(.white)
+                        .frame(minWidth: 50)
+                    if selectedOption == "Clips" {
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .matchedGeometryEffect(id: "Rectangle", in: namespace)
+                            .frame(width: 50, height: 2)
+                            .foregroundStyle(.green)
+                    }
+                }
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        selectedOption = "Clips"
+                    }
+                }
+                
+                ZStack(alignment: .bottom) {
+                    Text("Events")
+                        .padding(.bottom,10)
+                        .foregroundStyle(.white)
+                        .frame(minWidth: 50)
+                    if selectedOption == "Events" {
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .matchedGeometryEffect(id: "Rectangle", in: namespace)
+                            .frame(width: 50, height: 2)
+                            .foregroundStyle(.green)
+                    }
+                }
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        selectedOption = "Events"
+                    }
+                }
+            }
+            .padding(.top, 10)
+            
+            Spacer()
         }
         .padding()
     }
